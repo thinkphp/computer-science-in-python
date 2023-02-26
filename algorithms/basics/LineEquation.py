90d1498210d83c487b98c4fcaeb2c84d7d204ad7
Adrian Statescu <mergesortv@gmail.com>
2023-02-26 16:11:05 +0200

    Update LineEquation.py

--- algorithms/basics/LineEquation.py
+++ algorithms/basics/LineEquation.py
@@ -1,17 +1,24 @@
-#
+	#
 # General Equation Line: ax + by + c = 0
 # y - y1 = m (x - x1) with slope
 # y = m x + c
+# a = y1-y2
+# b = x2 - x1
+# c = x2y1 - x1y2
 #
 class Point():
+
     def __init__(self, x, y, name):
+    
         self.name = name
         self.x = x
         self.y = y
+        
     def __repr__(self):
         return self.name + '(' + str(self.x) + "," + str(self.y) + ')'
 
 def computeSlope(x1, y1, x2, y2):
+
     return (y2-y1)/(x2-x1)
 
 def func():
@@ -25,7 +32,12 @@ def func():
     print(A)
     print(B)
 
-    c = - slope * A.x + A.y
+    c = B.x * A.y - A.x * B.y
 
-    print("y = %f * x + %f"%(slope,c))
+    print("y = %.2f * x + %.2f"%(slope, c))
+    
+    print("y - %.2f = %.2f x - %.2f %.2f " % (A.y, slope, slope, A.x))
+    
+    print("y = %.2f x - %.2f %.2f + %.2f " % (slope, slope, A.x, A.y))
+    
 func()
