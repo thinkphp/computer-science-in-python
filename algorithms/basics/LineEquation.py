c515e42782a48b0ff5cd2fcc40bfc577bd698160
Adrian Statescu <mergesortv@gmail.com>
2023-02-27 13:26:54 +0200

    Update LineEquation.py

--- algorithms/basics/LineEquation.py
+++ algorithms/basics/LineEquation.py
@@ -1,4 +1,4 @@
-#
+
 # General Equation Line: ax + by + c = 0
 #
 # y - y1 = m (x - x1) where m = (y2-y1)/(x2-x1)
@@ -17,7 +17,9 @@ class Point():
     def __init__(self, x, y, name):
 
         self.name = name
+        
         self.x = x
+        
         self.y = y
 
     def __repr__(self):
@@ -36,12 +38,14 @@ def func():
     #Find the equation of the line that passes through the point (x, y) and has a slope
     slope = computeSlope(A.x, A.y, B.x, B.y)
 
-    print("Gradiend = ", slope)
+
     print(A)
     print(B)
+    print("Gradiend = ", slope)
 
     c = A.y - slope * A.x
-    print("First method:")
+    
+    print("First method (y = mx + c):")
     print("y = %.2f * x + %.2f"%(slope, c))
     
     print("Second method:")
@@ -49,11 +53,11 @@ def func():
 
     print("y = %.2f x - %.2f * %.2f + %.2f " % (slope, slope, A.x, A.y))
 
-    print("y = %.2f x %f " % (slope, -slope * A.x + A.y))
+    print("y = %.2f x + %f " % (slope, -slope * A.x + A.y))
     a = A.y - B.y
     b = B.x - A.x
-    c = (B.x * A.y) - (A.x * B.y)
+    c = (A.x * B.y) - (B.x * A.y)
     
-    print("Third method:")    
+    print("Third method (finding the coeffs: a, b, c):")    
     print("Equation Line(ax+by+c=0) => %.2f x+ %.2f y+ %.2f=0"%(a,b,c))
 func()
