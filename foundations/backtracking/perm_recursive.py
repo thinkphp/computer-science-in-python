5204821c09d9956f948751c81ec646c474f07468
Adrian Statescu <mergesortv@gmail.com>
2023-03-10 13:18:08 +0200

    Update perm_recursive.py

--- foundations/backtracking/perm_recursive.py
+++ foundations/backtracking/perm_recursive.py
@@ -1,54 +1,24 @@
-def init(level):
-
-    global stack
-    stack[ level ] = 0
-
-def succ(level):
-
-    global n, stack
-
-    if stack[ level ] < stack[ level - 1] + 1:
-
-       stack[ level ] += 1
-       return True
-
-    return False
-
-def valid(level):
-
-    return True
-
-def sol(level):
-
-    global n
-    return level == n
-
-def printf():
-
-    global n, stack
-
-    mx = max(stack)
-    for i in range(1, mx + 1):
-        print('{', end='')
-        for j in range(1, n + 1):
-            if stack[ j ] == i:
-               print(j, end = ',')
-        print('\b}', end='')
-    print()
-
-def bk(level):
-
-    init(level)
-    while succ(level) is True:
-        if valid(level) is True:
-            if sol(level) is True:
-                printf()
-            else:
-                bk(level + 1)
-
-def main():
-    global stack, n
-    n = 3
-    stack = [0] * (n + 1)
-    bk(1)
-main()
+def func():  
+  def printf() -> None:
+      for i in range(1, n+1):
+          print(stack[i], end = " ")
+      print() 
+        
+  def valid(level) -> bool:
+    for i in range(1, level):
+        if stack[level] == stack[i]:
+          return False
+    return True      
+        
+  def perm(level) -> None:
+      if level > n:
+         printf()
+      else:
+         for i in range(1, n+1):
+           stack[level] = i
+           if valid(level):              
+               perm(level+1)
+  n = 4
+  stack = [0] * (n + 1)
+  perm(1)
+func()  
