47e85c0a043115f8ba2f92725e556f426ed180fa
Adrian Statescu <mergesortv@gmail.com>
2023-07-11 18:22:45 +0300

    Update triangle-problem.py

--- foundations/dynamic-programming/triangle-problem.py
+++ foundations/dynamic-programming/triangle-problem.py
@@ -2,15 +2,15 @@ def fn():
 
     n = int(input())
 
-    Tri = [[0 for j in range(n+1)] for i in range(n+1)]
+    Tri = [ [0 for j in range( n + 1 ) ] for i in range( n + 1 ) ]
 
-    c = [[0 for j in range(n+1)] for i in range(n+1)]
+    c = [[0 for j in range( n + 1 ) ] for i in range( n + 1 ) ]
 
     for i in range(1,n+1):
 
-        for j in range(1,i+1):
+        for j in range( 1, i + 1 ):
 
-            Tri[i][j] = int(input())
+            Tri[ i ][ j ] = int( input() )
 
     for i in range(1,n+1):
 
@@ -21,6 +21,7 @@ def fn():
         print()
 
     for j in range(1,n+1):
+        
         c[n][j] = Tri[n][j]
 
     for i in range(n - 1, 0, -1):
@@ -28,11 +29,13 @@ def fn():
         for j in range(1, i + 1):
 
             if c[ i + 1 ][ j ] > c[ i + 1 ][ j + 1 ]:
+                
                c[ i ][ j ] = Tri[ i ][ j ] + c[ i + 1 ][ j ]
+                
             else:
+                
                c[ i ][ j ] = Tri[ i ][ j ] + c[ i + 1 ][ j + 1 ]
 
 
     print(c[1][1])
 fn()
-
