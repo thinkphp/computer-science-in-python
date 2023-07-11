854b6fc03913a74965edfcf4609c2f90e51c0fcf
Adrian Statescu <mergesortv@gmail.com>
2023-07-11 20:51:44 +0300

    Update triangle-problem.py

--- foundations/dynamic-programming/triangle-problem.py
+++ foundations/dynamic-programming/triangle-problem.py
@@ -2,15 +2,17 @@ def fn():
 
     n = int(input())
 
-    Tri = [ [0 for j in range( n + 1 ) ] for i in range( n + 1 ) ]
+    Tri = [[0 for _ in range(n+1)] for _ in range(n+1)]
 
-    c = [[0 for j in range( n + 1 ) ] for i in range( n + 1 ) ]
+    c = [[0 for _ in range(n+1)] for _ in range(n+1)]
+
+    road = [[0 for _ in range(n+1)] for _ in range(n+1)]
 
     for i in range(1,n+1):
 
-        for j in range( 1, i + 1 ):
+        for j in range(1,i+1):
 
-            Tri[ i ][ j ] = int( input() )
+            Tri[i][j] = int(input())
 
     for i in range(1,n+1):
 
@@ -21,7 +23,6 @@ def fn():
         print()
 
     for j in range(1,n+1):
-        
         c[n][j] = Tri[n][j]
 
     for i in range(n - 1, 0, -1):
@@ -29,13 +30,26 @@ def fn():
         for j in range(1, i + 1):
 
             if c[ i + 1 ][ j ] > c[ i + 1 ][ j + 1 ]:
-                
+
                c[ i ][ j ] = Tri[ i ][ j ] + c[ i + 1 ][ j ]
                 
-            else:
-                
-               c[ i ][ j ] = Tri[ i ][ j ] + c[ i + 1 ][ j + 1 ]
+               road[ i ][ j ] = j
 
+            else:
+
+               c[ i ][ j ] = Tri[ i ][ j ] + c[ i + 1 ][ j + 1 ]
+               
+               road[ i ][ j ] = j + 1
 
     print(c[1][1])
+
+    i = j = 1
+
+    while i <= n:
+
+        print( Tri[ i ][ j ], end = " " )
+
+        j = road[ i ][ j ]
+
+        i += 1
 fn()
