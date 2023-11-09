74bcf3fe628ec52ea424bfd15c456d27704c3d09
Adrian Statescu <mergesortv@gmail.com>
2023-11-09 11:38:15 +0200

    Create hello.world.py

---
+++ Gems /Tkinter /hello.world.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+window.title("PythonExamples.org")
+window.geometry("300x200")
+
+label = tk.Label(window, text="Enter input value")
+label.pack()
+
+# Function to insert value at beginning in Entry field
+def insert_value_in_entry():
+    entry.insert(2, "Hello World ")
+
+# Create an Entry field
+entry = tk.Entry(window)
+entry.pack()
+
+# Create a button
+button = tk.Button(window, text="Insert", command=insert_value_in_entry)
+button.pack()
+
+# Run the application
+window.mainloop()
