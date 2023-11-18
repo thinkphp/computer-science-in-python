f35f700c326a1a039e86fa55440a6db04bbba960
Adrian Statescu <mergesortv@gmail.com>
2023-11-18 22:41:24 +0200

    Create polymorphism2W3.py

---
+++ Gems /OOP /polymorphism /polymorphism2W3.py
@@ -0,0 +1,27 @@
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
