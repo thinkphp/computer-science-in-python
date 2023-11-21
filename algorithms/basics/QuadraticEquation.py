6d6f39ec5d29f309f4cc7a38869ba2cc7a1a62a7
Adrian Statescu <mergesortv@gmail.com>
2023-11-21 10:16:51 +0200

    Update QuadraticEquation.py

--- algorithms/basics/QuadraticEquation.py
+++ algorithms/basics/QuadraticEquation.py
@@ -1,30 +1,26 @@
-import math
-nl = list(input().split(' '))
-a = int(nl[0])
-b = int(nl[1])
-c = int(nl[2])
+A,B,C=map(int,input().split())
  
-if a == 0 and b == 0:
-	if c != 0:
-		print(0)
-		exit()
-	print(-1)
-	exit()
-	
-if a == 0:
-	print(1)
-	print("%10.5f" % (-c/b))
-	exit()
-	
-sqrt = b*b - 4*a*c
-if sqrt == 0:
-	print(1)
-	print("%10.5f" % (-b/2/a))
-elif sqrt < 0:
-	print(0)
-else:
-	print(2)
-	roots = [(math.sqrt(sqrt)-b)/2/a, (-math.sqrt(sqrt)-b)/2/a]
-	roots.sort()
-	print("%10.5f" % roots[0])
-	print("%10.5f" % roots[1])
+if A!=0:
+    if B*B-4*A*C<0:
+        print(0)
+    elif B*B-4*A*C==0:
+        print(1)
+        ANS=-B/(2*A)
+        print(ANS)
+    else:
+        print(2)
+        ANS=[(-B-(B*B-4*A*C)**(1/2))/(2*A)]
+        ANS.append((-B+(B*B-4*A*C)**(1/2))/(2*A))
+        ANS.sort()
+        print(ANS[0])
+        print(ANS[1])
+elif A==0:
+    if B!=0:
+        print(1)
+        ANS=(-C)/B
+        print(ANS)
+    else:
+        if C==0:
+            print(-1)
+        else:
+            print(0)
