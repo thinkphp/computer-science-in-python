79493db1c8cbc063338a673394e5f896cb2629d8
Adrian Statescu <mergesortv@gmail.com>
2023-11-26 19:49:08 +0200

    Create FirstDegreeEquation.py

---
+++ algorithms/basics/FirstDegreeEquation.py
@@ -0,0 +1,18 @@
+def FirstDegreeEquation(a, b):
+    if a == 0:
+        if b == 0:
+            print("Ecuatia are o infinitate de solutii!")
+        else:
+            print("Ecuatia nu are solutii")
+    else:
+        if b == 0:
+
+            print("x = 0")
+        else:
+            print("x = ", -b/a)
+def main():
+  a = 1
+  b = 5
+  b = 3
+  FirstDegreeEquation(a,b,c)
+main()  
